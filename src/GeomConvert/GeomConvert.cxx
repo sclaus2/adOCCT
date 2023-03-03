e3e93633bbcd5a94327364b63e746dd19ffff009
Mladen Banovic <mladen.banovic@dlr.de>
2023-03-03 16:42:45 +0100

    Avoid using memcpy in GeomConvert.cxx

--- src/GeomConvert/GeomConvert.cxx
+++ src/GeomConvert/GeomConvert.cxx
@@ -753,7 +753,10 @@ public:
 
   GeomConvert_reparameterise_evaluator (const Standard_Real thePolynomialCoefficient[3])
   {
-    memcpy (myPolynomialCoefficient, thePolynomialCoefficient, sizeof(myPolynomialCoefficient));
+    //memcpy (myPolynomialCoefficient, thePolynomialCoefficient, sizeof(myPolynomialCoefficient));
+    for (int i = 0; i < 3; ++i) {
+      myPolynomialCoefficient[i] = thePolynomialCoefficient[i];
+    }
   }
 
   virtual void Evaluate (const Standard_Integer theDerivativeRequest,
