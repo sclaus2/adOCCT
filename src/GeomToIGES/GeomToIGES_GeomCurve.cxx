e69123b993b1dd5e5ca5987fd4fa106196450ed9
Mladen Banovic <mladen.banovic@dlr.de>
2023-05-12 17:01:18 +0200

    Make TKIGES compilable (after myadouble introduction)

--- src/GeomToIGES/GeomToIGES_GeomCurve.cxx
+++ src/GeomToIGES/GeomToIGES_GeomCurve.cxx
@@ -576,7 +576,7 @@ Handle(IGESData_IGESEntity) GeomToIGES_GeomCurve::TransferCurve
     //Trimmed ellipse. To avoid huge weights in B-Spline first rotate it and then convert
     Handle(Geom_Ellipse) copystart = Handle(Geom_Ellipse)::DownCast (start->Copy());
     gp_Ax2 pos = copystart->Position();
-    copystart->SetPosition (pos.Rotated (pos.Axis(), gp_Ax3 (pos).Direct() ? Udeb : 2 * M_PI - Udeb));
+    copystart->SetPosition (pos.Rotated (pos.Axis(), gp_Ax3 (pos).Direct() ? Udeb : Standard_Real(2 * M_PI - Udeb)));
     Handle(Geom_BSplineCurve) Bspline;
     //:q3 abv 17 Mar 99: use GeomConvert_ApproxCurve for precise conversion
     const Handle(Geom_Curve)& aCopy = copystart; // to avoid ambiguity
