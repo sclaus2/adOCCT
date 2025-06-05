2f97e537aafd73ebbd5d300fbcb8428dc4b6dd21
Mladen Banovic <mladen.banovic@dlr.de>
2025-06-05 13:42:18 +0200

    Use std::copy in math_VectorBase

--- src/math/math_VectorBase.cxx
+++ src/math/math_VectorBase.cxx
@@ -9,9 +9,12 @@ math_VectorBase<Standard_Real>& math_VectorBase<Standard_Real>::Initialized(
     Length() != theOther.Length(),
     "math_VectorBase::Initialized() - input vector has wrong dimensions");
   // memmove(&Array.ChangeFirst(), &theOther.Array.First(), sizeof(TheItemType) * Array.Length());
+  /*
   for (int i = 0; i < Array.Length(); ++i)
   {
     Array[i] = theOther.Array[i];
   }
+  */
+  std::copy(theOther.Array.cbegin(), theOther.Array.cend(), Array.begin());
   return *this;
 }
