b79d57326786913db5a1d1b07e587abef8ecaf0e
Mladen Banovic <mladen.banovic@dlr.de>
2026-06-08 14:16:01 +0200

    Add notice to Standard_Adouble.hxx

--- src/Standard/Standard_Adouble.hxx
+++ src/Standard/Standard_Adouble.hxx
@@ -1,3 +1,15 @@
+// Created on: 2023-12-21
+// Created by: Mladen Banovic
+// Copyright (c) German Aerospace Center (DLR e.V.)
+//
+// This file is part of Open CASCADE Technology software library.
+//
+// This library is free software; you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License version 2.1 as published
+// by the Free Software Foundation, with special exception defined in the file
+// OCCT_LGPL_EXCEPTION.txt. Consult the file LICENSE_LGPL_21.txt included in OCCT
+// distribution for complete text of the license and disclaimer of any warranty.
+
 #ifndef _Standard_Adouble_HeaderFile
 #define _Standard_Adouble_HeaderFile
 
